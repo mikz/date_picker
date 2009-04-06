6506323353c82d7db04dbf19fd1fa65aa4054a37
mikz <mikz@7f78e5a9-1134-46b1-9fc8-7df727ab615c>
2009-04-06 20:50:44 +0000

    index with status
    git-svn-id: svn://localhost/date_picker/trunk@9 7f78e5a9-1134-46b1-9fc8-7df727ab615c

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,16 @@
 class WelcomeController < ApplicationController
   def index
+    @begin = Date.parse("2009-06-29")
+    @end   = Date.parse("2009-09-13")
+    @dates = {}
+    Day.find(:all,:include => :user).each { |day|
+      @dates[day.date] ||= []
+      @dates[day.date].push day
+    }
+    @max = 0
+    @dates.each_pair do |date,days|
+      @max = days.count if days.count > @max
+    end
   end
 
 end
